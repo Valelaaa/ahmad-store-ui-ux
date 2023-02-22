a8fa458a990be0d1bbcab8aa1a493ac7fc86aff1
Ivan <kyurkchyu10@gmail.com>
2023-02-22 18:21:42 +0200

    Fixed

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header class = "header">
+    <div class = "info">
+      <div class = "address">КОДРИЛОР 16, МУН. КИШИНЁВ</div>
+      <div class = "phone">+373 22 155 155<i class="bi-chevron-down"></i></div>
+      <div class="language">
+        <div class="language-ro">
+          RO
+        </div>
+        <div class="language-ro">
+          RU
+        </div>
+      </div>
+
+    </div>
+    <b-navbar toggleable = "lg" type = "dark" class = "bg-white" variant = "info">
+
+    </b-navbar>
+  </header>
+</template>
+
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Header',
+
+}
+</script>
+
+<style>
+
+</style>
